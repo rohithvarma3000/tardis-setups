50ded7592569918f997eee3ba1348480ac9692ff
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2022-07-14 17:32:57 +0530

    Test Aoife Paper

--- tardis-setups/setup_utils/config.py
+++ tardis-setups/setup_utils/config.py
@@ -1,4 +1,6 @@
 import os
+import re
+from setup_utils.download import download_from_url
 
 
 def find_file(name, path):
@@ -13,13 +15,23 @@ def config_modifier(conf):
     conf["montecarlo"]["last_no_of_packets"] = 1.0e5
     conf["montecarlo"]["no_of_virtual_packets"] = 3
 
-    conf["atom_data"] = os.path.join(
-        os.path.expanduser("~"), "Downloads", "tardis-data", conf["atom_data"]
+    print("Config")
+    url_check = re.search(
+        r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})",
+        conf["atom_data"],
     )
+    print(url_check)
+    if url_check:
+        conf["atom_data"] = download_from_url(conf["atom_data"])
 
-    if os.path.exists(conf["atom_data"]) == False:
-        conf["atom_data"] = find_file(conf["atom_data"], "/")
-        if os.path.exists(conf["atom_data"]) == False:
-            raise
+    else:
+        conf["atom_data"] = os.path.join(
+            os.path.expanduser("~"), "Downloads", "tardis-data", conf["atom_data"]
+        )
+
+    # if os.path.exists(conf["atom_data"]) == False:
+    #     conf["atom_data"] = find_file(conf["atom_data"], "/home")
+    #     if os.path.exists(conf["atom_data"]) == False:
+    #         raise Exception("Atomic dataset not found")
 
     return conf
--- tardis-setups/setup_utils/download.py
+++ tardis-setups/setup_utils/download.py
@@ -4,10 +4,19 @@ import os
 
 def download_from_url(url):
     try:
+        print("Started http")
         r = requests.get(url)
+        print("Request complete")
+        if r.status_code % 100 == 4:
+            raise Exception("File not found or url not authorised")
+        if r.status_code % 100 == 5:
+            raise Exception("Unable to download file due to server error")
+
         fname = r.headers.get("Content-Disposit").split("filename=")[1]
     except AttributeError:
         fname = url.split("/")[-1].split("?")[0]
 
     file_path = os.path.join(os.path.expanduser("~"), "Downloads", "tardis-data", fname)
     open(file_path, "wb").write(r.content)
+
+    return file_path
